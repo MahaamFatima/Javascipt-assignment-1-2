d6b805c0fd73e96066daf0a044234de5ad1c5223
Mahaam Fatima <36655436+MahaamFatima@users.noreply.github.com>
2024-09-30 19:50:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//Chapter 1*//
+//* Alert will show a pop up message on wesite showing messages or the alert() function is used to display a pop-up message or dialog box to the user.//*
+alert("Welcome to my website");
+
+
+//ALERT("Hello World!")  // It will give an error of ALERT is undefined//*
+
+alert("Hello, World!");
+//* A proper java script statement to write alert is*//
+alert("Hey there! I am Mahaam");
+//* In this statement an alert function displays a dialog box with a specified message,  Hey there! I am Mahaam When this statement is executed: A pop-up dialog box appears on the screen with the message "Hey There! I am Mahaam"*//
+//*The script execution will pause until the user clicks "OK" to close the alert box.*//
+
+//Chapter 2*//
+var message =("Hello World!");
+alert(message);
+var age;
+age=("25");
+alert(age);
+var greeting=("Good Morning!");
+alert(greeting);
+greeting=("Good Evening!");
+alert(greeting);
+var favouritecolor="Blue"
+alert("My Favourite color is "+ (favouritecolor))
